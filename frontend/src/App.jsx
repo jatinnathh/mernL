3cc1a8894a00efedd063c99d9616dde8032d9a33
jatinnathh <jatinnath1111@gmail.com>
2026-02-25 12:45:37 +0530

    home page added rate limitter ui and navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,21 @@
 import React from 'react'
-import Homepage  from './pages/homepage'
+import Homepage from './pages/homepage'
 import CreatePage from './pages/createpage'
 import NoteDetailPage from './pages/notedetailpage'
 import { Route, Routes } from 'react-router'
 import toast, { Toaster } from 'react-hot-toast'
 const App = () => {
   return (
-    <div>
+    <div data-theme="coffee">
       <Toaster />
-     <button onClick={() => toast.success("Con")}>
-  Show Toast
-</button>
+
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/notes/:id" element={<NoteDetailPage />} />
-
       </Routes>
-
     </div>
   )
 }
 
-export default App
+export default App  
--- frontend/src/pages/homepage.jsx
+++ frontend/src/pages/homepage.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Navbar from '../components/Navbar'
+import RateLimitedUI from '../components/RateLimitedUI'
 const Homepage = () => {
+
+  const [isRateLimited, setIsRateLimited] = useState(true)
   return (
-    <div>
-      Home Page
+    <div className='min-h-screen'>
+      <Navbar />
+
+      {isRateLimited && <RateLimitedUI />}
     </div>
   )
 }
